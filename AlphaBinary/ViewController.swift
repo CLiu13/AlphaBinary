87b4caa986f1ab63285ebc96a7159db1a5e089fd
Charlie Liu <CLiu13@users.noreply.github.com>
2017-05-12 09:11:51 -0400

    Made UILabel copyable

--- AlphaBinary/ViewController.swift
+++ AlphaBinary/ViewController.swift
@@ -8,9 +8,54 @@
 
 import UIKit
 
+class CopyableLabel: UILabel {
+    
+    override public var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        isUserInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(
+            target: self,
+            action: #selector(showMenu(sender:))
+        ))
+    }
+    
+    override func copy(_ sender: Any?) {
+        UIPasteboard.general.string = text
+        UIMenuController.shared.setMenuVisible(false, animated: true)
+    }
+    
+    func showMenu(sender: Any?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return (action == #selector(copy(_:)))
+    }
+}
+
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var returnLabel: UILabel!
+    @IBOutlet weak var returnLabel: CopyableLabel!
     
     @IBOutlet weak var textField: UITextField!
     
@@ -118,6 +163,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.textField.delegate = self
         
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard)))
+        
+        returnLabel.isUserInteractionEnabled = true
         
     }
 
